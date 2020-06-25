9cbe9706e82ee1aecfd3cb6820ddffcb8c5028c9
Furkan Pamuk <furkan.pamuk@web.de>
2020-06-26 01:12:36 +0200

    read rules from path

--- src/thesis/main/Main.java
+++ src/thesis/main/Main.java
@@ -1,10 +1,22 @@
 package thesis.main;
 
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
+import org.eclipse.core.runtime.CoreException;
+
+import crypto.rules.CrySLRule;
+import thesis.helpers.RuleReader;
+
+
 public class Main {
+	private static String CRYPTO_API_PATH = "C:/cygwin64/home/furka/rootDev/Thesis/res/repos/Crypto-API-Rules/BouncyCastle-JCA/src";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException, CoreException {
 		// TODO Auto-generated method stub
-
+		RuleReader rr = new RuleReader();
+		ArrayList<CrySLRule> list = (ArrayList<CrySLRule>) rr.readRules(CRYPTO_API_PATH);
+		System.out.println(list.size());
 	}
 
 }
