a748a5917f43bcdb38bd6e8876028969c45a04ff
Furkan Pamuk <furkan.pamuk@web.de>
2020-07-03 00:15:53 +0200

    getMethods, getNumerofMethods, getNumberOfParameters implemented in Counter Class

--- src/thesis/helpers/Counter.java
+++ src/thesis/helpers/Counter.java
@@ -3,29 +3,35 @@ package thesis.helpers;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import org.eclipse.core.runtime.CoreException;
 
+import crypto.rules.CrySLMethod;
 import crypto.rules.CrySLRule;
+import crypto.rules.TransitionEdge;
 
 public class Counter {
-	
-	private ArrayList<CrySLRule> rules; 
+
+	private ArrayList<CrySLRule> rules;
+	private HashSet<CrySLMethod> methods;
 	private String api, path;
-	
+
 	public Counter(String api, String path) throws MalformedURLException, CoreException {
 		RuleReader rr = new RuleReader();
 		this.path = path;
 		this.api = api;
 		this.rules = (ArrayList<CrySLRule>) rr.readRules(path);
+		this.parseMethods();
 	}
-	
+
 	/**
 	 * counts amounts of rules for each case and return hashmap
-	 * */
+	 */
 	public HashMap<String, Integer> getOccurences() {
-		int o,c,f,p,r,u; o = c = f = r = p = u = 0;
-		
+		int o, c, f, p, r, u;
+		o = c = f = r = p = u = 0;
+
 		for (CrySLRule rule : this.rules) {
 			o += rule.getObjects().size();
 			c += rule.getConstraints().size();
@@ -41,11 +47,46 @@ public class Counter {
 		map.put("Predicates", p);
 		map.put("Required", r);
 		map.put("UsagePatterns", u);
-		
 		return map;
 	}
+
+
+	/*
+	 * returns a list of parameter numbers for all methods
+	 */
+	public ArrayList<Integer> getNumberOfParameters() {
+		ArrayList<Integer> iParams = new ArrayList<Integer>();
+		for (CrySLMethod method : this.methods) {
+			iParams.add(method.getParameters().size());
+		} 
+		return iParams;
+	}
 	
+	public int getNumberOfMethods(){
+		return this.methods.size();
+	}
+
+	/*
+	 * returns all methods in CrySLRule (removes duplicates)
+	 */
+	public void parseMethods() {
+		HashSet<CrySLMethod> set = new HashSet<CrySLMethod>();
+		for (CrySLRule rule : this.rules) {
+			for (TransitionEdge edge : rule.getUsagePattern().getEdges()) {
+				for (CrySLMethod method : edge.getLabel()) {
+					set.add(method);
+				}
+			}
+		}
+		this.methods = set;
+	}
+
+	//GETTERS
 	public ArrayList<CrySLRule> getRules() {
 		return this.rules;
+	}
+	
+	public HashSet<CrySLMethod> getMethods(){
+		return this.methods;
 	}
 }
--- src/thesis/main/Main.java
+++ src/thesis/main/Main.java
@@ -1,26 +1,24 @@
 package thesis.main;
 
-import java.net.MalformedURLException;
-
-import org.eclipse.core.runtime.CoreException;
-
 import crypto.rules.CrySLRule;
 import thesis.helpers.Counter;
 
 
 public class Main {
-	private static String CRYPTO_API_PATH = "C:/cygwin64/home/furka/rootDev/repos/Crypto-API-Rules/BouncyCastle-JCA/src";
+	private static String CRYPTO_API_1 = "C:/cygwin64/home/furka/rootDev/repos/Crypto-API-Rules/BouncyCastle-JCA/src";
+	private static String CRYPTO_API_2 = "C:/cygwin64/home/furka/rootDev/repos/Crypto-API-Rules/BouncyCastle/src";
 
-	public static void main(String[] args) throws MalformedURLException, CoreException {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		Counter c = new Counter("BouncyCastle-JCA", CRYPTO_API_PATH);
+		Counter c = new Counter("BouncyCastle-JCA", CRYPTO_API_1 + "/tmp");
+		Counter c2 = new Counter("BouncyCastle", CRYPTO_API_2);
 		
-		System.out.println("Amount of Rules for BouncyCastle-JCA: " + c.getRules().size());
-		System.out.println(c.getOccurences());
+//		System.out.println("Amount of Rules for BouncyCastle-JCA: " + c.getRules().size());
+//		System.out.println(c.getOccurences());
 		
-//		CrySLRule r = c.getRules().get(0);
-		
-//		System.out.println(r.getConstraints().get(0).getLocation());
+		CrySLRule r = c.getRules().get(0);
+		System.out.println("Number of total Methods: " + c.getNumberOfMethods());
+		System.out.println("Number of total Parameters: " + c.getNumberOfParameters());
 	
 	}
 
