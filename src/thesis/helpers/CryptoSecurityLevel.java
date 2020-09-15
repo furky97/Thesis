2fd05a3e67255e3c4335c718ab0df7e3dcc2446d
Furkan Pamuk <furkan.pamuk@web.de>
2020-09-15 17:31:19 +0200

    updated security grads

--- src/thesis/helpers/CryptoSecurityLevel.java
+++ src/thesis/helpers/CryptoSecurityLevel.java
@@ -22,29 +22,29 @@ public class CryptoSecurityLevel {
 		this.assignments.put("MD5", 2);
 		this.assignments.put("RSA", 3);
 		this.assignments.put("RC5", 3);
-		this.assignments.put("DES", 3);
-		this.assignments.put("3DES", 3);
-		this.assignments.put("SKIPJACK", 3);
+		this.assignments.put("RC6", 4);
+		this.assignments.put("AES", 2);
+		this.assignments.put("DES", 2);
+		this.assignments.put("3DES", 2);
+		this.assignments.put("SKIPJACK", 2);
 		this.assignments.put("EC", 4);
 		this.assignments.put("DH", 4);
+		this.assignments.put("DSA", 4);
+		this.assignments.put("ECDSA", 4);
 		this.assignments.put("ElGamal", 4);
 		this.assignments.put("McEliece", 5);
 		this.assignments.put("McEliecePointcheval", 5);
 		this.assignments.put("McElieceKobaraImai", 5);
 		this.assignments.put("McElieceFujisaki", 5);
-		this.assignments.put("Blowfish", 2);
+		this.assignments.put("FrodoKEM-1344", 5);
+		this.assignments.put("FrodoKEM-976", 5);
 		this.assignments.put("Twofish", 4);
 		this.assignments.put("Camellia", 4);
-		this.assignments.put("Shacal-2", 2);
-		this.assignments.put("Shacal2", 2);
-		this.assignments.put("Tnepres", 2);
-		this.assignments.put("RIJNDAEL", 3);
+		this.assignments.put("RIJNDAEL", 4);
 		this.assignments.put("Serpent", 4);
-		this.assignments.put("DHIESwithAES-CBC", 2);
-		this.assignments.put("RIJNDAEL", 2);
-		this.assignments.put("ECIESwithAES-CBC", 2);
+		this.assignments.put("SHA-0", 2);
 		this.assignments.put("SHA-1", 2);
-		this.assignments.put("SHA-2", 3);
+		this.assignments.put("SHA-2", 4);
 		this.assignments.put("SHA-3", 4);
 		
 		this.ref.put(1, "VW (Very Weak)");
@@ -54,6 +54,24 @@ public class CryptoSecurityLevel {
 		this.ref.put(5, "US (Unconditionally Secure)");
 		
 		
+	}
+
+	
+	//GETTERS and SETTERS
+	public HashMap<String, Integer> getAssignments() {
+		return assignments;
+	}
+
+	public void setAssignments(HashMap<String, Integer> assignments) {
+		this.assignments = assignments;
+	}
+
+	public HashMap<Integer, String> getRef() {
+		return ref;
+	}
+
+	public void setRef(HashMap<Integer, String> ref) {
+		this.ref = ref;
 	}
 	
 	
